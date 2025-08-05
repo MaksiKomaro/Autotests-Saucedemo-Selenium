17784681035d0b31460183901930d36e13645c15
QA Maksim Komarov <komaro.qa@gmail.com>
2025-08-06 02:19:27 +0300

    Update start_page.py

--- pages/start_page.py
+++ pages/start_page.py
@@ -10,7 +10,7 @@ class StartPage(BasePage):
     HEADER_TEXT = 'Swag Labs'
     NO_USERNAME_TEXT = 'Epic sadface: Username is required'
     NO_PSWRD_TEXT = 'Epic sadface: Password is required'
-    WRONG_CREDS = 'Epic sadface: Username and password do not match any user in this service'
+    WRONG_CREDS_TEXT = 'Epic sadface: Username and password do not match any user in this service'
 
     HEADER = (By.CLASS_NAME, 'login_logo')
     USER_FIELD = (By.ID, 'user-name')
